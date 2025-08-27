629d44f61e5bda743f900336ccdb529e91fa77a1
mishrasuryakant <mishrasuryakant001@gmail.com>
2025-08-27 11:33:50 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,13 @@
+x,y=map(float,input("enter two numbers ").split())
+
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*y)
+print("division:",x/y)
+
+
+print("*********************************************************")
+
+a=input("Enter your first name " )
+b=input("Enter your last name " )
+print(f"hello,{a} {b} ! welcome to python program")
